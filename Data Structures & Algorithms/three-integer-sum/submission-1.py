3b0126ee2c3b4b16a68357e555d68d926f1611ed
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2026-03-31 23:52:10 +0800

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        pos = {}
+        for i in range(len(nums)):
+            pos[nums[i]] = i
+        res = []
+        ref = {}
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                s = nums[i]+nums[j]
+                find = -s
+                if find in pos and pos[find] > j:
+                    r = (nums[i], nums[j], find)
+                    if r in ref:
+                        continue
+                    ref[r] = True
+                    res.append(list(r))
+        return res
+
