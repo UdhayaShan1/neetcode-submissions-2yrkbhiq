a3f4b974a29cb5a773dbc171328c7c8285cf82df
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2026-04-27 19:12:49 +0800

    Update: valid-tree - submission-10

--- Data Structures & Algorithms/valid-tree/submission-10.py
+++ Data Structures & Algorithms/valid-tree/submission-10.py
@@ -6,24 +6,23 @@ class Solution:
         for i in edges:
             adjList[i[0]].append(i[1])
             adjList[i[1]].append(i[0])
-        
+        print(adjList)
         vis = {}
         def dfs(node, prev):
-            if node in vis:
-                return False
-
             vis[node] = True
-            res = True
             for i in adjList[node]:
                 if i == prev:
                     continue
-                res = res and dfs(i, node)
-            return res
+                if i in vis:
+                    return True
+                if dfs(i, node):
+                    return True
+            return False
         for i in range(n):
             if i in vis:
                 continue
-            if not dfs(i, -1):
+            res = dfs(i, -1)
+            if res:
+                #print(i)
                 return False
-        return len(edges) == n-1
-
-        
+        return len(vis) == n and len(edges) == n-1
