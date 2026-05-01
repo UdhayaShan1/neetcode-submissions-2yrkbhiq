edf184e9cee161a29e3af5c8b89226fc5f38117b
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2026-05-01 11:04:29 +0800

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = {}
+        indeg = {}
+        for i in range(numCourses):
+            adjList[i] = []
+            indeg[i] = 0
+        for i in prerequisites:
+            adjList[i[1]].append(i[0])
+            indeg[i[0]] += 1
+        print(adjList, indeg)
+        q = deque()
+        for i in indeg:
+            if indeg[i] == 0:
+                q.append(i)
+        path = []
+        while q:
+            curr = q.popleft()
+            path.append(curr)
+            for i in adjList[curr]:
+                indeg[i] -= 1
+                if indeg[i] == 0:
+                    q.append(i)
+        return len(path) == numCourses
+        
+        
