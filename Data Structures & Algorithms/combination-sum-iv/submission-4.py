e2eb4182c6fd2ad866febab2ad597186b78ac8b3
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2026-05-04 10:22:54 +0800

    Add: combination-sum-iv - submission-4

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        def cache2(func):
+            dp = {}
+            def wrapper(*args, **kwargs):
+                key = (args, tuple(sorted(kwargs.items())))
+                if key in dp:
+                    return dp[key]
+                dp[key] = func(*args, **kwargs)
+                return dp[key]
+            return wrapper
+        @cache2
+        def dfs(n):
+            if n == 0:
+                return 1
+            res = 0
+            for i in nums:
+                if n-i >= 0:
+                    res += dfs(n-i)
+            return res
+        return dfs(target)
