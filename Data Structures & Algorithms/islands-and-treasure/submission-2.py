9b4ad12520bb58b64c60b19175b1be031da22157
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2026-05-09 22:59:43 +0800

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        q = deque()
+        d = {}
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    q.append((i, j))
+                    d[(i, j)] = 0
+        #print(q)
+        while q:
+            curr = q.popleft()
+            for k in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
+                new_i = curr[0]+k[0]
+                new_j = curr[1]+k[1]
+                if new_i < 0 or new_j < 0 or new_i >= len(grid) or new_j >= len(grid[0]) or grid[new_i][new_j] == -1 or (new_i, new_j) in d:
+                    continue
+                d[(new_i, new_j)] = 1+d[curr]
+                q.append((new_i, new_j))
+        #print(d)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2**31-1:
+                    grid[i][j] = d.get((i, j), grid[i][j])
+            
+
+                
+                
