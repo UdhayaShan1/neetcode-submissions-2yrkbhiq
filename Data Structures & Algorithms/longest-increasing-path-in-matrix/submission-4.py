4b99006438194ed3bb7545764e6c3be3ce1f6225
Udhaya <39399745+UdhayaShan1@users.noreply.github.com>
2026-05-23 04:01:04 +0800

    Add: longest-increasing-path-in-matrix - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        sys.setrecursionlimit(9999999)
+        from functools import cache
+        @cache
+        def dfs(i, j):
+            res = 1
+            for k in [(0, -1), (0, 1), (1, 0), (-1, 0)]:
+                new_i = i+k[0]
+                new_j = j+k[1]
+                if new_i < 0 or new_j < 0 or new_i >= len(matrix) or new_j >= len(matrix[0]) or matrix[new_i][new_j] <= matrix[i][j]:
+                    continue
+                res = max(res, dfs(new_i, new_j)+1)
+            return res
+        res = 1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                res = max(res, dfs(i, j))
+        return res
+        
+                
